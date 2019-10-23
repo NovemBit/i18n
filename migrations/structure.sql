c0ac97a1268051b366f94da57a6d4a460ee52d3f
diazoxide <aaron.yor@gmail.com>
2019-10-23 12:07:27 +0400

    Update migration of structure.

--- migrations/structure.sql
+++ migrations/structure.sql
@@ -1,15 +1,14 @@
--- DROP TABLE IF EXISTS `i18n_translations`;
-
 CREATE TABLE `i18n_translations` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` int(11) DEFAULT NULL,
-  `from_language` varchar(2) COLLATE utf8mb4_bin DEFAULT NULL,
-  `to_language` varchar(2) COLLATE utf8mb4_bin DEFAULT NULL,
-  `source` longtext COLLATE utf8mb4_bin,
-  `translate` longtext COLLATE utf8mb4_bin,
-  `level` int(11) DEFAULT NULL,
-  `created_at` int(11) DEFAULT NULL,
-  `updated_at` int(11) DEFAULT NULL,
+  `id` mediumint(8) NOT NULL AUTO_INCREMENT,
+  `type` int(3) NOT NULL,
+  `from_language` char(2) COLLATE utf8mb4_bin NOT NULL,
+  `to_language` char(2) COLLATE utf8mb4_bin NOT NULL,
+  `source` text COLLATE utf8mb4_bin,
+  `translate` text COLLATE utf8mb4_bin,
+  `level` int(2) NOT NULL,
+  `created_at` int(10) unsigned DEFAULT NULL,
+  `updated_at` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `type` (`type`,`from_language`,`to_language`,`source`(100),`level`)
-) ENGINE=InnoDB AUTO_INCREMENT=24988 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  KEY `main_combo` (`type`,`from_language`,`to_language`,`source`(100),`level`),
+  KEY `language` (`from_language`,`to_language`)
+) ENGINE=InnoDB AUTO_INCREMENT=8381113 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
