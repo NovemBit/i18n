d7fe5ef9113e99846d599ae82980bd18876d4733
diazoxide <aaron.yor@gmail.com>
2019-10-24 17:02:43 +0400

    Fix translator interface saveModels issue

--- src/component/translation/interfaces/Translator.php
+++ src/component/translation/interfaces/Translator.php
@@ -7,14 +7,15 @@ namespace NovemBit\i18n\component\translation\interfaces;
 interface Translator
 {
 
-    public function translate(array $texts) : array;
+    public function translate(array $texts): array;
 
-    public function doTranslate(array $texts) : array;
+    public function doTranslate(array $texts): array;
 
     public function saveModels(
         $translations,
         $level = 0,
-        $overwrite = false
+        $overwrite = false,
+        &$result = []
     ): void;
 
     public function getModels(
