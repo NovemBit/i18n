51bb8ad1be923388581fa2f88c90f396f520f78f
diazoxide <aaron.yor@gmail.com>
2019-10-28 16:20:56 +0400

    Improve interface of Translation

--- src/component/translation/interfaces/Translation.php
+++ src/component/translation/interfaces/Translation.php
@@ -3,7 +3,18 @@
 
 namespace NovemBit\i18n\component\translation\interfaces;
 
+use NovemBit\i18n\component\translation\method\interfaces\Method;
+use NovemBit\i18n\component\translation\type\HTML;
+use NovemBit\i18n\component\translation\type\interfaces\Text;
+use NovemBit\i18n\component\translation\type\interfaces\URL;
 
+/**
+ *
+ * @property URL $url
+ * @property Text $text
+ * @property HTML $html
+ * @property Method $method
+ * */
 interface Translation
 {
     public function getFromLanguage() : string;
