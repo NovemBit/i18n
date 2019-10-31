206a455e18e1fe0e493d68c531a8f2646c1d7722
diazoxide <aaron.yor@gmail.com>
2019-10-31 11:03:23 +0400

    Fix Ajax provided content on editor mode

--- src/component/request/assets/js/xhr.js
+++ src/component/request/assets/js/xhr.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
     function parseURL(url) {
         let parser = document.createElement('a'),
@@ -6,7 +6,7 @@
             queries, split, i;
         parser.href = url;
         queries = parser.search.replace(/^\?/, '').split('&');
-        for( i = 0; i < queries.length; i++ ) {
+        for (i = 0; i < queries.length; i++) {
             split = queries[i].split('=');
             searchObject[split[0]] = split[1];
         }
@@ -21,23 +21,35 @@
             hash: parser.hash
         };
     }
-    function addParameterToURL(url,key,value){
-        url += (url.split('?')[1] ? '&':'?') + key+'='+value;
+
+    function addParameterToURL(url, key, value) {
+        url += (url.split('?')[1] ? '&' : '?') + key + '=' + value;
         return url;
     }
+
     let valid_hosts = [
         //    'test.com'
     ];
     let original_xhr = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function(){
+    XMLHttpRequest.prototype.open = function () {
         let req_parsed = parseURL(arguments[1]);
         let cur_parsed = parseURL(window.location.href);
-        if(req_parsed.host === cur_parsed.host && valid_hosts.indexOf(req_parsed.host)){
+        if (req_parsed.host === cur_parsed.host && valid_hosts.indexOf(req_parsed.host)) {
             arguments[1] = addParameterToURL(
                 arguments[1],
                 window.novembit.i18n.language_query_key,
                 window.novembit.i18n.current_language
             );
+
+            if (window.novembit.i18n.editor.is_editor === true) {
+                arguments[1] = addParameterToURL(
+                    arguments[1],
+                    window.novembit.i18n.prefix + '-' + window.novembit.i18n.editor.query_key,
+                    1
+                );
+            }
+
+            console.log(arguments[1]);
         }
         original_xhr.apply(this, arguments);
     }
