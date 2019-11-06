815beca6c85adb642a4818530b514ba34619417f
diazoxide <aaron.yor@gmail.com>
2019-11-06 10:27:54 +0400

    Added interface for XML type.

---
+++ src/component/translation/type/interfaces/XML.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * HTML translation type interface file
+ * php version 7.2.10
+ *
+ * @category Component\Translation\Type\Interface
+ * @package  Component\Translation\Type\Interface
+ * @author   Aaron Yordanyan <aaron.yor@gmail.com>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @version  GIT: @1.0.1@
+ * @link     https://github.com/NovemBit/i18n
+ */
+
+namespace NovemBit\i18n\component\translation\type\interfaces;
+
+/**
+ * HTML translation type interface
+ *
+ * @category Component\Translation\Type\Interface
+ * @package  Component\Translation\Type\Interface
+ * @author   Aaron Yordanyan <aaron.yor@gmail.com>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link     https://github.com/NovemBit/i18n
+ */
+interface XML extends Type
+{
+
+    public function getHelperAttributes() : bool;
+
+    public function setHelperAttributes(bool $status) : void;
+
+    public function addBeforeParseCallback(callable $callback):void;
+
+    public function addAfterParseCallback(callable $callback):void;
+
+
+}
