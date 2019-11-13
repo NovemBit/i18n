da4b5a475509ede54154f59ac5718f5cc495c050
diazoxide <aaron.yor@gmail.com>
2019-11-13 14:16:04 +0400

    Update db structure.

--- migrations/structure.sql
+++ migrations/structure.sql
@@ -1,14 +1,15 @@
 CREATE TABLE `i18n_translations` (
-  `id` mediumint(11) NOT NULL AUTO_INCREMENT,
-  `type` int(3) NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `type` int(3) unsigned NOT NULL,
   `from_language` char(2) COLLATE utf8mb4_bin NOT NULL,
   `to_language` char(2) COLLATE utf8mb4_bin NOT NULL,
   `source` text COLLATE utf8mb4_bin,
   `translate` text COLLATE utf8mb4_bin,
-  `level` int(2) NOT NULL,
+  `level` int(2) unsigned NOT NULL,
   `created_at` int(10) unsigned DEFAULT NULL,
   `updated_at` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `main_combo` (`type`,`from_language`,`to_language`,`source`(100),`level`),
+  KEY `main_combo` (`type`,`from_language`,`to_language`,`source`(200),`level`),
+  KEY `main_combo_re` (`type`,`from_language`,`to_language`,`translate`(200),`level`),
   KEY `language` (`from_language`,`to_language`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
