e1b0cb0d8a38fb3f901a298ef3a225772f402bbb
diazoxide <aaron.yor@gmail.com>
2019-11-19 11:14:07 +0400

    Added HTML fragment type of translation

---
+++ src/component/translation/type/HTMLFragment.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Translation component
+ * php version 7.2.10
+ *
+ * @category Component\Translation\Type
+ * @package  Component\Translation\Type
+ * @author   Aaron Yordanyan <aaron.yor@gmail.com>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @version  GIT: @1.0.1@
+ * @link     https://github.com/NovemBit/i18n
+ */
+
+namespace NovemBit\i18n\component\translation\type;
+
+use NovemBit\i18n\component\translation\interfaces\Translation;
+use \NovemBit\i18n\system\parsers;
+
+/**
+ * HTML type for translation component
+ *
+ * @category Component\Translation\Type
+ * @package  Component\Translation\Type
+ * @author   Aaron Yordanyan <aaron.yor@gmail.com>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link     https://github.com/NovemBit/i18n
+ *
+ * @property Translation context
+ */
+class HTMLFragment extends HTML implements interfaces\HTMLFragment
+{
+    /**
+     * {@inheritdoc}
+     * */
+    public $name = 'html_fragment';
+
+    /**
+     * Model class name of ActiveRecord
+     *
+     * @var \NovemBit\i18n\component\translation\models\Translation
+     * */
+    public $model_class = models\HTML::class;
+
+    protected $parser_type = parsers\interfaces\XML::HTML_FRAGMENT;
+
+}
