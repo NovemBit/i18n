54e2143f0ce15206b6189c9549ad8c2e81644c98
diazoxide <aaron.yor@gmail.com>
2019-11-28 18:31:20 +0400

    Updated structure of DB.

--- migrations/structure.sql
+++ migrations/structure.sql
@@ -1,15 +1,17 @@
-CREATE TABLE `i18n_translations` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `type` int(3) unsigned NOT NULL,
-  `from_language` char(2) COLLATE utf8mb4_bin NOT NULL,
-  `to_language` char(2) COLLATE utf8mb4_bin NOT NULL,
-  `source` text COLLATE utf8mb4_bin,
-  `translate` text COLLATE utf8mb4_bin,
-  `level` int(2) unsigned NOT NULL,
-  `created_at` int(10) unsigned DEFAULT NULL,
-  `updated_at` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `main_combo` (`type`,`from_language`,`to_language`,`source`(200),`level`),
-  KEY `main_combo_re` (`type`,`from_language`,`to_language`,`translate`(200),`level`),
-  KEY `language` (`from_language`,`to_language`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+CREATE TABLE `i18n_translations`
+(
+    `id`             int(10) unsigned                                  NOT NULL AUTO_INCREMENT,
+    `type`           int(3)                                            NOT NULL,
+    `from_language`  char(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+    `to_language`    char(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+    `source`         text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+    `translate`      text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+    `level`          int(2)                                            NOT NULL,
+    `source_hash`    varchar(32) DEFAULT NULL,
+    `translate_hash` varchar(32) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `main_combo_hash` (`type`, `from_language`, `to_language`, `source_hash`, `level`),
+    KEY `main_combo_re_hash` (`type`, `from_language`, `to_language`, `translate_hash`, `level`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = latin1;
