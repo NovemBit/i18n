f6d596f4937dc82249a22f086a9d5468325c05a0
diazoxide <aaron.yor@gmail.com>
2020-03-12 18:52:58 +0400

    Update uses and docs

--- src/component/localization/interfaces/Localization.php
+++ src/component/localization/interfaces/Localization.php
@@ -1,15 +1,18 @@
 <?php
 
-
 namespace NovemBit\i18n\component\localization\interfaces;
 
+use NovemBit\i18n\component\localization\countries\interfaces\Countries;
 use NovemBit\i18n\component\localization\languages\interfaces\Languages;
+use NovemBit\i18n\component\localization\regions\interfaces\Regions;
 use NovemBit\i18n\system\interfaces\Component;
 
 /**
  * Languages component interface
  *
  * @property Languages $languages
+ * @property Regions $regions
+ * @property Countries $countries
  * */
 interface Localization extends Component
 {
