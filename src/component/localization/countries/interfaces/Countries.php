28ded06ff5470b4ebb6b778bc4feadc37845b130
diazoxide <aaron.yor@gmail.com>
2020-03-12 18:52:32 +0400

    Updated interface for Countries type

--- src/component/localization/countries/interfaces/Countries.php
+++ src/component/localization/countries/interfaces/Countries.php
@@ -1,10 +1,15 @@
 <?php
 
-
 namespace NovemBit\i18n\component\localization\countries\interfaces;
-
 
 interface Countries
 {
-
+    /**
+     * Get default country
+     *
+     * @param string|null $base_domain Base domain
+     *
+     * @return string
+     * */
+    public function getDefaultCountry(?string $base_domain = null): ?string;
 }
