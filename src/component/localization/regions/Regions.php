e0c77774ec1d09f69125d478f96aae1463d8cd8b
diazoxide <aaron.yor@gmail.com>
2020-03-12 18:49:33 +0400

    Extend Regions from LocalizationType and implements Region interface

--- src/component/localization/regions/Regions.php
+++ src/component/localization/regions/Regions.php
@@ -1,15 +1,17 @@
 <?php
 
-
 namespace NovemBit\i18n\component\localization\regions;
 
-
+use NovemBit\i18n\component\localization\Localization;
+use NovemBit\i18n\component\localization\LocalizationType;
 use NovemBit\i18n\system\Component;
 use NovemBit\i18n\system\helpers\Arrays;
 
-class Regions extends Component
+/**
+ * @property Localization $context
+ * */
+class Regions extends LocalizationType implements interfaces\Regions
 {
-    public $all;
 
     /**
      * @return array
@@ -22,21 +24,6 @@ class Regions extends Component
     }
 
     /**
-     * @param string      $key
-     * @param string      $by
-     * @param string|null $return
-     *
-     * @return mixed|null
-     */
-    public function getRegion(
-        string $key,
-        string $by,
-        ?string $return
-    ) {
-        return Arrays::find($this->all, $key, $by, $return);
-    }
-
-    /**
      * @param string $key
      * @param string $value
      *
@@ -45,5 +32,27 @@ class Regions extends Component
     public function getCountriesMap(string $key, string $value): array
     {
         return Arrays::map($this->all, $key, $value);
+    }
+
+    /**
+     * @param string|null $base_domain
+     * @return array|mixed|null
+     */
+    public function getConfig(?string $base_domain = null, ?string $value = null)
+    {
+        return $this->get($base_domain, 'domain', $value);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param string|null $base_domain Base domain
+     *
+     * @return string
+     */
+    public function getDefaultRegion(?string $base_domain = null): ?string
+    {
+        $config = $this->getConfig($base_domain);
+        return $config['name'] ?? null;
     }
 }
