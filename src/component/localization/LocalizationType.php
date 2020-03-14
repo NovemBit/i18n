38c6d74e3fdd356faa9f4f5a1fdd0715c8fabd8c
diazoxide <aaron.yor@gmail.com>
2020-03-14 14:57:54 +0400

    Add getByPrimary and getByContains methods

--- src/component/localization/LocalizationType.php
+++ src/component/localization/LocalizationType.php
@@ -16,14 +16,35 @@ abstract class LocalizationType extends Component
      *
      * @return mixed|null
      */
-    public function get(
+    public function getByPrimary(
         string $key,
         string $by,
         ?string $return = null
     ) {
-
         return Arrays::ufind($this->all, $key, $by, $return, function ($key, $value) {
-            if (is_string($value) && $key == $value) {
+            if (is_string($value) && $key === $value) {
+                return true;
+            } elseif (is_array($value) && isset($value[0]) && $value[0] === $key) {
+                return true;
+            }
+            return false;
+        });
+    }
+
+    /**
+     * @param string $key
+     * @param string $by
+     * @param string|null $return
+     *
+     * @return mixed|null
+     */
+    public function getByContains(
+        string $key,
+        string $by,
+        ?string $return = null
+    ) {
+        return Arrays::ufind($this->all, $key, $by, $return, function ($key, $value) {
+            if (is_string($value) && $key === $value) {
                 return true;
             } elseif (is_array($value) && in_array($key, $value)) {
                 return true;
