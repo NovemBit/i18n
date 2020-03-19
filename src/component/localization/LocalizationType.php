d298d5c53605a9507fdfa0ac64b2a037d618994a
diazoxide <aaron.yor@gmail.com>
2020-03-19 12:01:54 +0400

    LocalizationType getByPrimary return all elements feature

--- src/component/localization/LocalizationType.php
+++ src/component/localization/LocalizationType.php
@@ -13,43 +13,66 @@ abstract class LocalizationType extends Component implements interfaces\Localiza
      * @param string $key
      * @param string $by
      * @param string|null $return
+     * @param bool $all
      *
      * @return mixed|null
      */
     public function getByPrimary(
         string $key,
         string $by,
-        ?string $return = null
+        ?string $return = null,
+        bool $all = false
     ) {
-        return Arrays::ufind($this->all, $key, $by, $return, function ($key, $value) {
-            if (is_string($value) && $key === $value) {
-                return true;
-            } elseif (is_array($value) && isset($value[0]) && $value[0] === $key) {
-                return true;
-            }
-            return false;
-        });
+        return call_user_func_array(
+            [Arrays::class, $all ? 'ufindAll' : 'ufind'],
+            [
+                $this->all,
+                $key,
+                $by,
+                $return,
+                function ($key, $value) {
+                    if (is_string($value) && $key === $value) {
+                        return true;
+                    } elseif (is_array($value) && isset($value[0]) && $value[0] === $key) {
+                        return true;
+                    }
+                    return false;
+                }
+            ]
+        );
     }
 
     /**
      * @param string $key
      * @param string $by
      * @param string|null $return
+     * @param bool $all
      *
      * @return mixed|null
      */
     public function getByContains(
         string $key,
         string $by,
-        ?string $return = null
+        ?string $return = null,
+        bool $all = false
     ) {
-        return Arrays::ufind($this->all, $key, $by, $return, function ($key, $value) {
-            if (is_string($value) && $key === $value) {
-                return true;
-            } elseif (is_array($value) && in_array($key, $value)) {
-                return true;
-            }
-            return false;
-        });
+
+        return call_user_func_array(
+            [Arrays::class, $all ? 'ufindAll' : 'ufind'],
+            [
+                $this->all,
+                $key,
+                $by,
+                $return,
+                function ($key, $value) {
+                    if (is_string($value) && $key === $value) {
+                        return true;
+                    } elseif (is_array($value) && in_array($key, $value)) {
+                        return true;
+                    }
+                    return false;
+                }
+            ]
+        );
     }
 }
