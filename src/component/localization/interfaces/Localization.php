41b082eacd4b02f223a1a9d733c8e1f797625efa
diazoxide <aaron.yor@gmail.com>
2020-03-30 11:41:58 +0400

    Add getActiveDomain and getActiveLanguages methods to Localization interface

--- src/component/localization/interfaces/Localization.php
+++ src/component/localization/interfaces/Localization.php
@@ -17,4 +17,6 @@ use NovemBit\i18n\system\interfaces\Component;
 interface Localization extends Component
 {
     public function getGlobalDomains(): array;
+    public function getActiveDomain(string $language): ?string;
+    public function getActiveLanguages(?string $base_domain = null): array;
 }
