c3001909cc1ad0bf550578b0d889a2ebe47e50be
diazoxide <aaron.yor@gmail.com>
2020-04-03 12:43:15 +0400

    Change Rest method status constants types

--- src/component/rest/interfaces/Rest.php
+++ src/component/rest/interfaces/Rest.php
@@ -3,14 +3,14 @@
 
 namespace NovemBit\i18n\component\rest\interfaces;
 
-
 use NovemBit\i18n\system\interfaces\Component;
 
 interface Rest extends Component
 {
-    const STATUS_NONE = 0;
-    const STATUS_DONE = 1;
-    const STATUS_EMPTY = -1;
-    const STATUS_ERROR = -2;
+    public const STATUS_NONE = 0;
+    public const STATUS_DONE = 1;
+    public const STATUS_EMPTY = -1;
+    public const STATUS_ERROR = -2;
+
     public function start(): void;
 }
